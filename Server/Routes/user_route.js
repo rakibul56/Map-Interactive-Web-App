fb95e92543c681240bb62240514a5333416b6a58
Rakibul Islam <rakibulroyal@gmail.com>
2024-12-03 15:51:27 +0100

    User routes endpoint  added

--- Server/Routes/user_route.js
+++ Server/Routes/user_route.js
@@ -34,6 +34,28 @@ router.get("/api/users/:id", async (req, res) => {
   }
   
 })
+//logIn End points
+router.post("/api/users/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+     // Check if the user with the provided email exists
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(401).json({ message: "Email is incorrect" });
+    }
+
+    // Check if the provided password matches the stored password
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Password is incorrect" });
+    }
+    
+    res.status(200).json(user);
+  
+  } catch (error) {
+      res.status(500).send({ message: error.message });
+  }
+  
+})
 
 
 

8241faaef8f90ad8485eeb02f73165403d9a2008
Rakibul Islam <rakibulroyal@gmail.com>
2025-02-23 20:21:49 +0100

    About Page added

---
+++ Client/src/pages/About.jsx
@@ -0,0 +1,13 @@
+
+const About = () => {
+  return (
+    <div className="container mx-auto flex flex-col justify-center items-center ">
+      <div className=" container mx-auto flex flex-col justify-center items-center  ">
+        <h1 className=" p-4   font-bold">Rakibul Islam</h1>
+        <p> Technische Universität Chemnitz</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;

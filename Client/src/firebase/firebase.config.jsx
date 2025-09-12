d7abafd1dc8b5c4a6940345eb8e38021ab33399b
Rakibul Islam <rakibulroyal@gmail.com>
2025-09-12 14:45:37 +0200

    Add Firebase configuration for authentication and database

---
+++ Client/src/firebase/firebase.config.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtHyLT-XKb8zfTmbwwWBhPjdhbWZSi6DQ",
+  authDomain: "edufinder-6c4df.firebaseapp.com",
+  projectId: "edufinder-6c4df",
+  storageBucket: "edufinder-6c4df.appspot.com",
+  messagingSenderId: "504568895919",
+  appId: "1:504568895919:web:d34f04b9fba2677413f75a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;

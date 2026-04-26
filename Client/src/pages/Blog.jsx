c5fcd6acc09c60abda837f28e739053cb7435b1c
Rakibul Islam <rakibul.islam@s2019.tu-chemnitz.de>
2026-04-26 02:00:16 +0200

    blog_page_added

---
+++ Client/src/pages/Blog.jsx
@@ -0,0 +1,17 @@
+
+
+const Blog = () => {
+  return (
+    <div className="w-full ">
+      <div className=" container mx-auto flex flex-col justify-center items-center  ">
+        <h1 className=" p-4 m-4  font-bold">This page is Under Construction</h1>
+        <p className=" m-4 mx-auto ">
+          This is a Blog page where you can post your thought and vote other
+          post!!!!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
